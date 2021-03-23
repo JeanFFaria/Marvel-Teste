0b53c47d803a2dda35ebd9b6fe1ac078ebd2a72c
JeanFFaria <73616975+JeanFFaria@users.noreply.github.com>
2021-03-23 20:09:56 -0300

    arquivos monique

---
+++ app/src/main/java/com/example/marvelapp/ui/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.marvelapp.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.R
+import com.example.marvelapp.fragments.FragmentOne
+import com.example.marvelapp.fragments.FragmentTwo
+import com.example.marvelapp.fragments.adapter.MyPagerAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val viewPagerAdapter = MyPagerAdapter(childFragmentManager)
+        viewPagerAdapter.addFragment(FragmentOne(), "Comic")
+        viewPagerAdapter.addFragment(FragmentTwo(), "Heroes")
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+
+    }
+}
---
+++ app/src/main/java/com/example/marvelapp/ui/notifications/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.example.marvelapp.ui.notifications
+
+import androidx.fragment.app.Fragment
+import com.example.marvelapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile)
+
